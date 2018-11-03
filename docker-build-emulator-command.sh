d338a39cfec0504f5fc6f7d8dcd614d55f25c44f
alxndrpsclt <alxndr.psclt@gmail.com>
2018-11-03 17:06:16 +0100

    Initial commit

---
+++ docker-build-emulator-command.sh
@@ -0,0 +1,3 @@
+#docker run -ti --privileged -v /dev:/dev -v /home/alex/Documents/creations/prog/cluster/virtual-cluster/mountfolders/simonpi00:/root/.simonpi m0rf30/simonpi simonpi rpi-3 -i /root/.simonpi/2017-11-29-raspbian-stretch-lite.img -s 8
+
+docker run -ti --privileged -v /dev:/dev -v /home/alex/Documents/creations/prog/cluster/virtual-cluster/mountfolders/simonpi02:/root/.simonpi m0rf30/simonpi simonpi rpi-3 -i /root/.simonpi/2018-06-27-raspbian-stretch-lite.img -s 8
