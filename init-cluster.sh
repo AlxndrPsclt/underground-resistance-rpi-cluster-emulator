237ea9d3b574e61fa18aea7463f1f9b01e5a9627
alxndrpsclt <alxndr.psclt@gmail.com>
2018-11-05 18:56:30 +0100

    Adds first download support

--- init-cluster.sh
+++ init-cluster.sh
@@ -16,10 +16,7 @@ else
 		mkdir -p $NODE_VIRTUAL_DISK_FOLDER
 		echo "Created virtual folder disk for node underground-resistance-$i"
 
-		if [ -f $SEED_IMAGE_PATH ]; then
-			echo "$SEED_IMAGE_PATH will be copied to the virtual disk $i."
-			cp $SEED_IMAGE_PATH $NODE_VIRTUAL_DISK_FOLDER
-		else
+		if [ ! -f $SEED_IMAGE_PATH ]; then
 			echo "File $SEED_IMAGE_PATH does not exist. We will try to download the latest raspbian image."
 			wget -P $SEED_IMAGES_PATH https://downloads.raspberrypi.org/raspbian_lite_latest
 			unzip -d $SEED_IMAGES_PATH $SEED_IMAGES_PATH/raspbian_lite_latest
@@ -29,6 +26,10 @@ else
 			echo "Going forward with downloaded raspian image: $SEED_IMAGE_PATH"
 			echo "You might need to change your .env file to use this image next time instead of a new download"
 		fi
+
+		echo "$SEED_IMAGE_PATH will be copied to the virtual disk $i."
+		cp $SEED_IMAGE_PATH $NODE_VIRTUAL_DISK_FOLDER
+
 	done
 
 
