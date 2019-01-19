e74225c05e7a25ecbe298f8fdc7e531d1e999d28
Matthieu Tâche <mtache@cisco.com>
2019-01-19 17:59:20 +0100

    Fixed launch script

--- launch-cluster.sh
+++ launch-cluster.sh
@@ -1,7 +1,14 @@
 #! /usr/bin/env bash
 
-echo "Preparing the launch of the RPI emulator."
-echo "All config comes from the .env file"
+RED='\033[0;31m'
+BROWN='\033[0;33m'
+GREEN='\033[0;32m'
+L_GRAY='\033[0;37m'
+NC='\033[0m' # No Color
+
+
+echo -e "Preparing the launch of the RPI emulator.\n"
+echo -e "${L_GRAY}All config comes from the .env file${NC}"
 echo "Generating cluster-compose.yml file..."
 
 if [ ! -f .env ]; then
@@ -10,14 +17,16 @@ fi
 
 if [ ! -f cluster-compose.yml ]; then
 	touch cluster-compose.yml
+else
+	echo -e "${BROWN}cluster-compose.yml exists, stopping the stack...${NC}"
+	docker-compose -f cluster-compose.yml down
+	echo -e ""
 fi
 
 docker-compose -f ./generate-cluster-compose/generate-compose.yml up
-echo "cluster-compose.yml is ready."
-echo ""
+echo -e "${GREEN}cluster-compose.yml is ready.${NC}\n"
 
-echo "Preparing virtual disks..."
-
+echo -e "Preparing virtual disks..."
 
 source ./.env
 
@@ -26,7 +35,7 @@ if [ -d $CLUSTER_VIRTUAL_DISKS_FOLDER ]; then
 	read -p "Do you want to delete it and start with a new cluster?" -n 1 -r
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		echo "Deleting the content of $CLUSTER_VIRTUAL_DISKS_FOLDER."
+		echo -e "${RED}Deleting the content of $CLUSTER_VIRTUAL_DISKS_FOLDER.${NC}"
 		sudo rm -rf $CLUSTER_VIRTUAL_DISKS_FOLDER
 	fi
 fi
@@ -35,28 +44,29 @@ if [ ! -d $CLUSTER_VIRTUAL_DISKS_FOLDER ]; then
 	echo "The folder $CLUSTER_VIRTUAL_DISKS_FOLDER will be created to host the virtual disk file."
 	mkdir -p $CLUSTER_VIRTUAL_DISKS_FOLDER
 
-	for i in `seq -w 001 $NUMBER_OF_RPI_NODES`; do
-		NODE_VIRTUAL_DISK_FOLDER=$CLUSTER_VIRTUAL_DISKS_FOLDER/underground-resistance-$i
+	for i in `seq -w 1 $NUMBER_OF_RPI_NODES`; do
+		no=$(printf "%03d" $i)
+		NODE_VIRTUAL_DISK_FOLDER=$CLUSTER_VIRTUAL_DISKS_FOLDER/underground-resistance-$no
 		mkdir -p $NODE_VIRTUAL_DISK_FOLDER
-		echo "Created virtual folder disk for node underground-resistance-$i"
+		echo "Created virtual folder disk for node underground-resistance-$no"
 
 		if [ ! -f $SEED_IMAGE_PATH ]; then
-			echo "File $SEED_IMAGE_PATH does not exist. We will try to download the latest raspbian image."
-			wget -P $SEED_IMAGES_PATH https://downloads.raspberrypi.org/raspbian_lite_latest
+			echo -e "${BROWN}File $SEED_IMAGE_PATH does not exist. Downloading the latest raspbian image...${NC}"
+			wget -q --show-progress -P $SEED_IMAGES_PATH https://downloads.raspberrypi.org/raspbian_lite_latest
 			unzip -d $SEED_IMAGES_PATH $SEED_IMAGES_PATH/raspbian_lite_latest
-			SEED_IMAGE_FILENAME=`zipinfo -1 images/raspbian_lite_latest`
-			SEED_IMAGE_PATH="$SEED_IMAGES_PATH/$SEED_IMAGE_FILENAME"  #Will be created if doesn't exist
+			NEW_SEED_IMAGE_FILENAME=`zipinfo -1 images/raspbian_lite_latest`
+			SEED_IMAGE_PATH="$SEED_IMAGES_PATH/$NEW_SEED_IMAGE_FILENAME"  #Will be created if doesn't exist
 			rm $SEED_IMAGES_PATH/raspbian_lite_latest
-			echo "Going forward with downloaded raspian image: $SEED_IMAGE_PATH"
-			echo "You might need to change your .env file to use this image next time instead of a new download"
+			echo "${GREEN}Going forward with downloaded raspian image: $NEW_SEED_IMAGE_FILENAME.${NC}"
+			sed -i.bak s/$SEED_IMAGE_FILENAME/$NEW_SEED_IMAGE_FILENAME/g .env
 		fi
 
-		echo "$SEED_IMAGE_PATH will be copied to the virtual disk $i."
+		echo "$SEED_IMAGE_PATH will be copied to the virtual disk n°$i."
 		cp $SEED_IMAGE_PATH $NODE_VIRTUAL_DISK_FOLDER
 	done
 fi
 
-echo "Virtual disks ready."
-echo "Everything is ready, launching the virtual cluster."
+echo -e "${GREEN}Virtual disks ready."
+echo -e "Everything is ready, launching the virtual cluster.${NC}"
 
 docker-compose -f cluster-compose.yml up
